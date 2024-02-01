fe7acfefaba92d5fc94ecbac5d5cdf8c107758d6
dinadina1 <dinad9355@gmail.com>
2024-02-01 22:22:37 +0530

    iterate JSON over loops

--- script.js
+++ script.js
@@ -1,60 +1,36 @@
-// Question No 1 : For the given JSON iterate over all for loopa (for, for in, for of, forEach)
+// Question No 1 : Create your own resume data in JSON format
 
- //For in Loop
-const students = {
-  "info": [
-    { "id": 1, "name": "John Doe", "age": 18 },
-    { "id": 2, "name": "Jane Smith", "age": 17 },
-  ]};
-
-for (let i in students.info) {
-  console.log("Id : "+students.info[i].id);
-  console.log("Name : "+students.info[i].name);
-  console.log("Age : "+students.info[i].age);
+const resume = {
+  "Name":"Dinakaran",
+  "Qualification":"BCA",
+  "Passedout Year":"2022",
+  "Father Name":"Muthu",
+  "Mother Name":"Meena",
+  "Age":"22",
+  "Date of Birth":"15-11-2002",
+  "Place":"Chennai",
+  "Occupation":"Student",
+  "CGPA":"8.5",
+  "Skill set":["Javascript","HTML","CSS","C Programming"],
+  "Passion":"Web development"
 }
 
-//=================================================================
-
-//For of Loop
-const students1 = {
-  "info": [
-    { "id": 1, "name": "John Doe", "age": 18 },
-    { "id": 2, "name": "Jane Smith", "age": 17 },
-  ]};
-
-for (let i of students1.info) {
-  console.log("Id : "+i.id);
-  console.log("Name : "+i.name);
-  console.log("Age : "+i.age);
+for(var data in resume){
+console.log(`${data} : ${resume[data]}`);
 }
 
-//=================================================================
-
-//For Each Loop
- const students2 = {
-    "info": [
-      { "id": 1, "name": "John Doe", "age": 18 },
-      { "id": 2, "name": "Jane Smith", "age": 17 },
-    ]};
-  
-    students2.info.forEach(function(info) {
-    console.log("Id : "+info.id);
-    console.log("Name : "+info.name);
-    console.log("Age : "+info.age);
-  });
-
-  //================================================================
-
-//For Loop
-const students3 = {
-  "info": [
-    { "id": 1, "name": "John Doe", "age": 18 },
-    { "id": 2, "name": "Jane Smith", "age": 17 },
-  ]};
-
-  for(var i=0;i<students3.info.length;i++) {
-  console.log("Id : "+students3.info[i].id);
-  console.log("Name : "+students3.info[i].name);
-  console.log("Age : "+students3.info[i].age);
-};
-
+/*
+Result : 
+Name : Dinakaran
+Qualification : BCA
+Passedout Year : 2022
+Father Name : Muthu
+Mother Name : Meena
+Age : 22
+Date of Birth : 15-11-2002
+Place : Chennai
+Occupation : Student
+CGPA : 8.5
+Skill set : Javascript,HTML,CSS,C Programming
+Passion : Web development
+*/
--- script1.js
+++ script1.js
@@ -1,6 +1,8 @@
-// Question No 2 : Create your own resume data in JSON format
+// Question No 2 : For the above JSON, iterate over all for loops (for, for in, for of, forEach)
 
-const resume = {
+ //For in Loop
+ 
+ let resume = {
     "Name":"Dinakaran",
     "Qualification":"BCA",
     "Passedout Year":"2022",
@@ -11,8 +13,150 @@ const resume = {
     "Place":"Chennai",
     "Occupation":"Student",
     "CGPA":"8.5",
-    "Skill set":["Javascript","HTML","Css","C Programming"]
+    "Skill set":["Javascript","HTML","CSS","C Programming"],
+    "Passion":"Web development"
+}  
+  for (let data in resume) {
+    console.log(`${data} : ${resume[data]}`);
+  }
+  
+  /*
+Result : 
+-------
+Name : Dinakaran
+Qualification : BCA
+Passedout Year : 2022
+Father Name : Muthu
+Mother Name : Meena
+Age : 22
+Date of Birth : 15-11-2002
+Place : Chennai
+Occupation : Student
+CGPA : 8.5
+Skill set : Javascript,HTML,CSS,C Programming
+Passion : Web development
+*/
+
+  //=================================================================
+  
+  //For of Loop
+
+  let resume1=[
+    {"Name":"Dinakaran"},
+    {"Qualification":"BCA"},
+    {"Passedout Year":"2022"},
+    {"Father Name":"Muthu"},
+    {"Mother Name":"Meena"},
+    {"Age":"22"},
+    {"Date of Birth":"15-11-2002"},
+    {"Place":"Chennai"},
+    {"Occupation":"Student"},
+    {"CGPA":"8.5"},
+    {"Skill set":["Javascript","HTML","CSS","C Programming"]},
+    {"Passion":"Web development"}
+]
+for (data of resume1) {
+    for(obj in data){
+        console.log(`${obj} : ${data[obj]}`);
+    }
 }
-for(var key in resume){
-console.log(key+" : "+resume[key]);
+
+  /*
+Result : 
+-------
+Name : Dinakaran
+Qualification : BCA
+Passedout Year : 2022
+Father Name : Muthu
+Mother Name : Meena
+Age : 22
+Date of Birth : 15-11-2002
+Place : Chennai
+Occupation : Student
+CGPA : 8.5
+Skill set : Javascript,HTML,CSS,C Programming
+Passion : Web development
+*/
+  
+  //=================================================================
+  
+  //For Each Loop
+
+  let resume2 = [{
+    "Name":"Dinakaran",
+    "Qualification":"BCA",
+    "Passedout Year":"2022",
+    "Father Name":"Muthu",
+    "Mother Name":"Meena",
+    "Age":"22",
+    "Date of Birth":"15-11-2002",
+    "Place":"Chennai",
+    "Occupation":"Student",
+    "CGPA":"8.5",
+    "Skill set":["Javascript","HTML","CSS","C Programming"],
+    "Passion":"Web development"
+}]
+resume2.forEach(element => {
+    for(data in element){
+        console.log(`${data} : ${element[data]}`);
+    }
+});
+
+/*
+Result : 
+-------
+Name : Dinakaran
+Qualification : BCA
+Passedout Year : 2022
+Father Name : Muthu
+Mother Name : Meena
+Age : 22
+Date of Birth : 15-11-2002
+Place : Chennai
+Occupation : Student
+CGPA : 8.5
+Skill set : Javascript,HTML,CSS,C Programming
+Passion : Web development
+*/
+  
+    //================================================================
+  
+  //For Loop
+
+  let resume3 = [{
+    "Name":"Dinakaran",
+    "Qualification":"BCA",
+    "Passedout Year":"2022",
+    "Father Name":"Muthu",
+    "Mother Name":"Meena",
+    "Age":"22",
+    "Date of Birth":"15-11-2002",
+    "Place":"Chennai",
+    "Occupation":"Student",
+    "CGPA":"8.5",
+    "Skill set":["Javascript","HTML","CSS","C Programming"],
+    "Passion":"Web development"
+}]
+
+for(let i=0;i<resume3.length;i++){
+    for(data in resume3[i]){
+        console.log(`${data} : ${resume3[i][data]}`);
+    }
 }
+
+/*
+Result : 
+-------
+Name : Dinakaran
+Qualification : BCA
+Passedout Year : 2022
+Father Name : Muthu
+Mother Name : Meena
+Age : 22
+Date of Birth : 15-11-2002
+Place : Chennai
+Occupation : Student
+CGPA : 8.5
+Skill set : Javascript,HTML,CSS,C Programming
+Passion : Web development
+*/
